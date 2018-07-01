96c644eef259f1fde8b86b1ae6bb01d362188517
Gary Preston <gary@mups.co.uk>
2018-07-01 17:37:03 +0100

    Add crude release packaging script.

---
+++ create_release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Crude packaging script for releases
+hash zip 2>/dev/null || { echo >&2 "zip required but not found.  Aborting."; exit 1; }
+hash git 2>/dev/null || { echo >&2 "zip required but not found.  Aborting."; exit 1; }
+
+VERSION=`git describe --tags`
+DATE=`date -u '+%Y%m%d%H%M%S'`
+RELEASE_ZIP="AcornElectron_${VERSION}_${DATE}.zip"
+
+######################################################################
+
+echo "Creating release zip in ${RELEASE_ZIP}"
+pushd sdcard > /dev/null
+
+zip ../"${RELEASE_ZIP}" acorn_electron.bin changelog.md empty.rom readme.md replay.ini
