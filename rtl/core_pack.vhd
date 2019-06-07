107f4dc42e6a402c2d245b5d88f2473eb724308f
Gary Preston <gary@mups.co.uk>
2019-06-07 02:02:33 +0100

    c_clk_ctl_for_video no longer a boolean

--- rtl/core_pack.vhd
+++ rtl/core_pack.vhd
@@ -47,7 +47,7 @@ library ieee;
 
 Package Core_Pack is
   constant c_clk_sys_divider      : natural := 32;   -- divide clk_sys down to 1MHz
-  constant c_clk_ctl_for_video    : boolean := true;
+  constant c_clk_ctl_for_video    : string := "SYNC";
   constant c_use_dram             : boolean := true; -- enabled DRAM block
   constant c_use_fileio           : boolean := true; -- enabled FILEIO block
   constant c_dram_late_start      : boolean := false;
