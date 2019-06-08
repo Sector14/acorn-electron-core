3b0e69004cdfec461c47e7617cbd794c0a1380b1
Gary Preston <gary@mups.co.uk>
2019-06-08 22:04:31 +0100

    Add local override for scr.
    Core needs a little extra optimisation to meet timing constraints with current implementation.

--- rmake.py
+++ rmake.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
 import runpy, sys
-sys.argv.append('--root=../../') # default is ../../
-sys.argv.append('--core=acorn_electron') # default is ../../
+sys.argv.append('--core=acorn_electron')
 runpy.run_path('../../replay_common/scripts/common.py')
-
